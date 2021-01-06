4ba852f885fd5bfe0ec153cd306036f30c653542
BlasteriskVon <red1000@rocketmail.com>
2021-01-05 22:59:20 -0500

    testing index.js

--- app/models/index.js
+++ app/models/index.js
@@ -3,7 +3,8 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
-var env = process.env.JAWSDB_URL || "development";
+var env = process.env.NODE_ENV || "development";
+console.log(env);
 var config = require(path.join(__dirname, '../../', 'config', 'config.json'))[env]; //initially the second argument was .. but that was app/config and led to errors
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
